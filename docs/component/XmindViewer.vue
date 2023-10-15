a3637a512116814b87cf931c11d26bcb1c3931bc
SimpleMw <907678446@qq.com>
2023-10-15 22:08:51 +0800

    deploy

--- docs/component/XmindViewer.vue
+++ docs/component/XmindViewer.vue
@@ -19,7 +19,7 @@ onMounted(async () => {
   const viewer = new XMindEmbedViewer({
     el: '#xmind-container', // HTMLElement | HTMLIFrameElement | string
     // 如果在中国大陆境内速度慢，可以添加的参数 `region: 'cn'` 改为使用 xmind.cn 的图库作为依赖。
-    region: 'cn' //optinal, global(default) or cn
+    // region: 'cn' //optinal, global(default) or cn
   })
   viewer.setStyles({
     width: '100%',
